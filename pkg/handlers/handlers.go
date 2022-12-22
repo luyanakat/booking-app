5b4dcf43f5faba39f36f3e2e9503868c506ab313
luyanakat <cuong02.master@gmail.com>
2022-12-22 14:50:09 +0700

    update html

--- pkg/handlers/handlers.go
+++ pkg/handlers/handlers.go
@@ -26,15 +26,20 @@ func NewHandle(r *Repository) {
 
 func (m *Repository) Home(w http.ResponseWriter, r *http.Request) {
 	remoteIp := r.RemoteAddr
+	userName := r.UserAgent()
+
 	m.App.Session.Put(r.Context(), "remote_ip", remoteIp)
+	m.App.Session.Put(r.Context(), "remote_user", userName)
 	render.WriteTemplate(w, "home.page.gohtml", &models.TemplateData{})
 }
 
 func (m *Repository) About(w http.ResponseWriter, r *http.Request) {
 	stringMap := make(map[string]string)
 	remoteIp := m.App.Session.GetString(r.Context(), "remote_ip")
+	userName := m.App.Session.GetString(r.Context(), "remote_user")
 
 	stringMap["remote_ip"] = remoteIp
+	stringMap["remote_user"] = userName
 	stringMap["name"] = "Nai"
 	render.WriteTemplate(w, "about.page.gohtml", &models.TemplateData{
 		StringMap: stringMap,
